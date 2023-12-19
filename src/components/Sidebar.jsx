eccdb9eababec56ff00c821574d1b6867ce2bfef
teymur <teymurhasanov100@gmail.com>
2023-12-19 16:50:41 +0400

    last

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,50 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate, NavLink } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { toast } from "react-hot-toast";
+import {
+  FaBars,
+  FaHome,
+  FaCommentAlt,
+  FaLock,
+  FaQuestion,
+  FaSignOutAlt,
+} from "react-icons/fa";
+import "../styles/CustomStyle.css";
 
 const Sidebar = () => {
   const { logout } = useAuth();
   const { auth } = useAuth();
-  const { name } = auth || null;
+  const { name } = auth || "";
   const navigate = useNavigate();
 
+  const [showMenu, setShowMenu] = useState("false");
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <aside className="h-full bg-gray-200 p-5">
-      <div className="mb-5 cursor-pointer" onClick={() => navigate("/")}>
-        <p>Application</p>
+    <aside className="relative h-full bg-gray-200 py-3">
+      <div className="mb-3 cursor-pointer px-2" onClick={toggleMenu}>
+        <FaBars />
       </div>
+      <div className={`${showMenu ? "block" : "hidden"}`}>
+        <nav className="flex flex-col items-start">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "active-nav-element" : "nav-element"
+            }
+          >
+            <FaHome className="mr-2" />
+            Home
+          </NavLink>
+          <NavLink
+            to="post"
+            className={({ isActive }) =>
+              isActive ? "active-nav-element" : "nav-element"
+            }
+          >
+            <FaCommentAlt className="mr-2" />
+            Post
+          </NavLink>
+          <NavLink
+            to="/private"
+            className={({ isActive }) =>
+              isActive ? "active-nav-element" : "nav-element"
+            }
+          >
+            <FaLock className="mr-2" />
+            Private Page
+          </NavLink>
+          <NavLink
+            to="*"
+            className={({ isActive }) =>
+              isActive ? "active-nav-element" : "nav-element"
+            }
+          >
+            <FaQuestion className="mr-2" />
+            Missing
+          </NavLink>
+        </nav>
 
-      <nav className="flex flex-col">
-        <NavLink to="/" className="mb-2 text-blue-600 hover:text-orange-500">
-          Home
-        </NavLink>
-        <NavLink to="post" className="mb-2 text-blue-600 hover:text-orange-500">
-          Post
-        </NavLink>
-        <NavLink
-          to="/private"
-          className="mb-2 text-blue-600 hover:text-orange-500"
-        >
-          Private Page
-        </NavLink>
-        <NavLink to="*" className="mb-2 text-blue-600 hover:text-orange-500">
-          Missing
-        </NavLink>
-      </nav>
-
-      <pre className="mt-3">{name}!</pre>
-
-      <button
-        className="mt-4 cursor-pointer rounded-lg bg-orange-500 px-6 py-2 text-white hover:bg-orange-600"
-        onClick={() => {
-          logout();
-          navigate("/login", { replace: true });
-          toast.success("İstifadəçi çıxış etdi", { duration: 1000 });
-        }}
-      >
-        Logout
-      </button>
+        <div className="absolute bottom-0 left-0 right-0 mt-3 flex flex-col items-center">
+          <pre>Hello {name}!</pre>
+          <button
+            className="mt-1 flex w-full cursor-pointer justify-center bg-orange-500 px-7 py-3 text-lg text-white hover:bg-orange-600"
+            onClick={() => {
+              logout();
+              navigate("/login", { replace: true });
+              toast.success("İstifadəçi çıxış etdi", { duration: 1000 });
+            }}
+          >
+            <FaSignOutAlt />
+          </button>
+        </div>
+      </div>
     </aside>
   );
 };
